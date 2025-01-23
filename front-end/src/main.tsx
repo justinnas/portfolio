75889774b793dce2f61f34e52ab2a8b20c2e5e33
Justinas <156369263+justinnas@users.noreply.github.com>
2025-01-24 01:39:06 +0200

    Deployment updates

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { ThemeProvider } from './theme/ThemeContext';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>
 		<ThemeProvider>
 			<RouterProvider router={router} />
--- front-end/src/router.tsx
+++ front-end/src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './routes/Home';
 import { Links } from './routes/Links';
 
 export const router = createBrowserRouter([
 	{ path: '/', element: <Home /> },
 	{ path: '/links', element: <Links /> },
-	{ path: '*', element: <Navigate to='/' replace /> },
+	//{ path: '*', element: <Navigate to='/' replace /> },
 ]);
