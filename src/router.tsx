85a4ac182d92eaeeece52475da40444b70aff894
Justinas <156369263+justinnas@users.noreply.github.com>
2025-01-24 00:03:40 +0200

    Structure cleanup

--- src/router.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { Home } from './routes/Home';
-import { Links } from './routes/Links';
-
-export const router = createBrowserRouter([
-	{ path: '/', element: <Home /> },
-	{ path: '/links', element: <Links /> },
-	{ path: '*', element: <Navigate to='/' replace /> },
-]);
